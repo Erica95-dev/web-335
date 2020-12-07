6aeed0072f721cf333903e1b4b196b4d8f4f3857
Ericadp.developer <ericadp.developer@gmail.com>
2020-12-06 20:26:27 -0500

    Create perry_calculator.py

---
+++ week 8/perry_calculator.py
@@ -0,0 +1,26 @@
+# Title:assignmet 8.3 Pyhton 
+#Author: Erica Perry
+#Date:12/07/2020
+#Modified: Erica Perry
+#Description: Python 
+
+first_name = 'Erica'
+
+last_name = 'Perry'
+
+print(first_name + ' ' + last_name)
+
+
+
+def add(num1,num2):
+  return 3
+
+def subtract(num4,num1):
+  return 3
+
+def divide (num8,num2):
+  return 4
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
